d53fe5a3c413dabd16980edbd673109d5dfc5fef
paulsunny90 <paulsunny87267@gmail.com>
2026-01-10 12:45:17 +0530

    admin contriler added

--- Server/Controller/Admin.controller.js
+++ Server/Controller/Admin.controller.js
@@ -1,12 +1,12 @@
-// import Adimin from "../models/admin.js";
+import Adimin from "../models/admin.js";
 
-// export const AdminLogin=(req,res)=>{
-//     try {
-//         const login = Adimin.create(req.body)
-//         res.json({login})
+export const AdminLogin=(req,res)=>{
+    try {
+        const login = Adimin.create(req.body)
+        res.json({login})
         
-//     } catch (error) {
-//         console.log(error)
-//     }
+    } catch (error) {
+        console.log(error)
+    }
 
-// }
+}
